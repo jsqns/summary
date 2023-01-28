92cce78567db7e80e637c7c9b833a1fe98b30b1d
Jsqns <852555310@qq.com>
2023-01-28 19:11:11 +0800

    update+

--- src/main/java/com/js/summary/service/impl/SummaryServiceImpl.java
+++ src/main/java/com/js/summary/service/impl/SummaryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.js.summary.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.js.summary.common.Result;
@@ -8,24 +9,26 @@ import com.js.summary.entity.Summary;
 import com.js.summary.handler.TestHandler;
 import com.js.summary.service.SummaryService;
 import com.js.summary.mapper.SummaryMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 /**
-* @author qns
-* @description 针对表【summary】的数据库操作Service实现
-* @createDate 2022-12-01 21:43:21
-*/
+ * @author qns
+ * @description 针对表【summary】的数据库操作Service实现
+ * @createDate 2022-12-01 21:43:21
+ */
 @Service
+@Slf4j
 public class SummaryServiceImpl extends ServiceImpl<SummaryMapper, Summary>
-    implements SummaryService{
+        implements SummaryService {
 
     @Resource
     private TestHandler testHandler;
 
-    public void getTestHandle(){
+    public void getTestHandle() {
         System.out.println(testHandler);
     }
 
@@ -35,6 +38,8 @@ public class SummaryServiceImpl extends ServiceImpl<SummaryMapper, Summary>
         wrapper.orderByAsc("id");
         wrapper.last("limit 1");
         Summary one = this.getOne(wrapper);
+        log.info("summary-info:{}", JSON.toJSONString(one));
+        log.error("summary-error:{}", JSON.toJSONString(one));
         List<Long> summaryList = one.getSummary();
         Long num = 100L;
         if (summaryList.contains(num)) {
